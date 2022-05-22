af212bb5f2160f32a86a4d7af8a9aecf3cc7883b
Thor <74827097+thorzambo@users.noreply.github.com>
2022-05-22 16:18:01 +0200

    Changes

--- Snake/src/GamePanel.java
+++ Snake/src/GamePanel.java
@@ -92,7 +92,12 @@ public class GamePanel extends JPanel implements ActionListener {
     }
 
     public void checkApple() {
+        if((x[0] == appleX) && (y[0] == appleY)) {
+            bodyParts++;
+            applesEaten++;
+            newApple();
 
+        }
     }
 
     public void checkCollisions() {
@@ -165,4 +170,5 @@ public class GamePanel extends JPanel implements ActionListener {
             }
         }
     }
+    
 }
